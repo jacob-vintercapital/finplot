55afc9c6764d1837007452985fa5ed98abdd111a
jacob-vintercapital <jacob@vinter.capital>
2019-01-03 06:54:50 +0100

    edit vol plot
    Signed-off-by: jacob-vintercapital <jacob@vinter.capital>

--- py/7-vcc_vol.py
+++ py/7-vcc_vol.py
@@ -15,7 +15,8 @@ ylabel_mca = 'Fraction of total market capitalization'
 mcafr_vcc_mat = mca_vcc_mat.div(mca_vcc_mat.sum(1), axis=0)
 
 # create matrixes
-mca_1 = pd.concat([m1, mca_vcc_mat[tkr_t3]], 1)
+mca_1 = pd.concat([mca_vcc_mat[tkr_t3], m1], 1)
+mca_1 = mca_1.loc[m1.index[1]:, ]
 mcafr_1 = mca_1.div(mca_vcc_mat.sum(1), axis=0)
 mca_1 = mca_1.loc[START2:]
 mcafr_1 = mcafr_1.loc[START2:]
@@ -25,6 +26,7 @@ mcafr_1.plot()
 plt.title(title_mca)
 plt.ylabel(ylabel_mca)
 plt.savefig('output/vcc/mca/mca_vcc_bsk1.png')
+
 
 # plot 2
 mcafr_1[tkr_t3].plot()
@@ -38,30 +40,38 @@ plt.savefig('output/vcc/mca/mca_vcc.png')
 
 title_vol = 'Trading volume'
 ylabel_vol = 'Fraction of total trading volume'
-
+ylabel_usd = 'Trading volume in USD'
 
 # create matrixes
-vol_1 = pd.concat([m1, vol_vcc_mat[tkr_t3]], 1)
+vol_1 = pd.concat([v1, vol_vcc_mat[tkr_t3]], axis=1)
+vol_1 = vol_1.loc[m1.index[1]:]
 volfr_1 = vol_1.div(vol_vcc_mat.sum(1), axis=0)
-vol_1 = vol_1.loc[START2:]
-volfr_1 = volfr_1.loc[START2:]
+
 
 # plot 1
 volfr_1.rolling(20).mean().plot()
 plt.title(title_vol)
 plt.ylabel(ylabel_vol)
 plt.savefig('output/vcc/vol/volfr_vcc_bsk1_smooth20.png')
+plt.close()
 
-# plot 2
+# plot 2 volfr top3 + index
 volfr_1[tkr_t3].rolling(20).mean().plot()
 plt.title(title_vol)
 plt.ylabel(ylabel_vol)
 plt.savefig('output/vcc/vol/volfr_vcc_smooth20.png')
+plt.close()
 
 # plot 3
 vol_1[tkr_t3].rolling(20).mean().plot()
 plt.title(title_vol)
-plt.ylabel('Trading volume')
+plt.ylabel(ylabel_usd)
 plt.savefig('output/vcc/vol/vol_vcc_smooth20.png')
+plt.close()
 
-
+# plot 3 log
+vol_1.rolling(20).mean().plot(logy=True)
+plt.title(title_vol + ' on a log scale')
+plt.ylabel(ylabel_usd)
+plt.savefig('output/vcc/vol/vol_vcc_smooth20_log.png')
+plt.close()
